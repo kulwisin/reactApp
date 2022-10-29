0657005d0fc4e5440111bf43214ac8e18a0c1cf4
kusingh <kulwinder_singh@gap.com>
2022-10-28 18:08:16 -0700

    Updates

--- src/app.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from './App';
-
-it('Information App', () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
